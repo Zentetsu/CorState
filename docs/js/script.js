31bbdbbb4db84693df405f2076a1b6b1998c3d0c
Zentetsu <10742751+Zentetsu@users.noreply.github.com>
2020-09-21 22:50:40 +0200

    Adding editable name for state and transition

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,3 +1,40 @@
+/*
+ * File: script.js
+ * Created Date: Friday, September 5th 2020, 9:52:35 pm
+ * Author: Zentetsu
+ *
+ * ----
+ *
+ * Last Modified:
+ * Modified By:
+ *
+ * ----
+ *
+ * Project: CorState
+ * Copyright (c) 2020 Zentetsu
+ *
+ * ----
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ----
+ *
+ * HISTORY:
+ * 2020-09-21	Zen	Adding editable name for state and transition
+ */
+
+
 // Init GRID
 var canvas = new fabric.Canvas('canvas', { selection: false });
 var canvasWidth =  window.innerWidth * 0.98;
@@ -36,7 +73,7 @@ function calcArrowAngle(x1, y1, x2, y2) {
 
 // Snap moved State or Transition to the GRID
 canvas.on('object:moving', function(options) {
-    if(options.target.type ==='state') {
+    if(options.target.type === 'state') {
         options.target.set({
             left: Math.round(options.target.left / grid) * grid,
             top: Math.round(options.target.top / grid) * grid
@@ -56,14 +93,44 @@ canvas.on('object:moving', function(options) {
             p.line.arrow.set({
                 angle: aa +90
             });
-        } else {
+
+            if(p.line.get('y1') === p.line.get('y2')) {
+                add_top = -10;
+                add_left = 0;
+            } else if(p.line.get('x1') === p.line.get('x2')) {
+                add_top = 0;
+                add_left = 30;
+            }
+
+            p.line.text.set({
+                left: (p.line.get('x1') + p.line.get('x2')) / 2 + add_left,
+                top: (p.line.get('y1') + p.line.get('y2')) / 2 + add_top
+            });
+        } else if(options.target.part === 'out') {
             p.line && p.line.set({ 'x2': Math.round(p.left / grid) * grid, 'y2': Math.round(p.top / grid) * grid});
             aa = calcArrowAngle(p.line.get('x1'), p.line.get('y1'), p.line.get('x2'), p.line.get('y2'));
 
             options.target.set({
                 angle: aa +90
             });
+
+            if(p.line.get('y1') === p.line.get('y2')) {
+                add_top = 10;
+                add_left = 0;
+            } else if(p.line.get('x1') === p.line.get('x2')) {
+                add_top = 0;
+                add_left = 30;
+            }
+
+            p.line.text.set({
+                left: (p.line.get('x1') + p.line.get('x2')) / 2 + add_left,
+                top: (p.line.get('y1') + p.line.get('y2')) / 2 + add_top
+            });
         }
+        // else if(options.target.part === "segment") {
+        //     p.circle && p.circle.set({ 'left': Math.round(p.left / grid) * grid, 'top': Math.round(p.top / grid) * grid - p.height / 2});
+        //     p.arrow && p.arrow.set({ 'left': Math.round(p.left / grid) * grid, 'top': Math.round(p.top / grid) * grid + p.height / 2});
+        // }
     }
 });
 
@@ -79,17 +146,40 @@ canvas.on('object:modified', function(options) {
     });
 });
 
+canvas.on('selection:created', function(options) {
+    console.log(options.target.type)
+    if(options.target.part === "text") {
+        document.getElementById("rename").value = options.target.text;
+    } else {
+        document.getElementById("rename").value = options.target.text.text;
+    }
+});
+
+canvas.on('selection:updated', function(options) {
+    console.log(options.target.type)
+    if(options.target.part === "text") {
+        document.getElementById("rename").value = options.target.text;
+    } else {
+        document.getElementById("rename").value = options.target.text.text;
+    }
+});
+
+canvas.on('selection:cleared', function(options) {
+    document.getElementById("rename").value = "";
+});
+
 // Adding State to the GRID
 function addState() {
     //TODO add ID
     id = 0;
 
-    canvas.add(new fabric.Rect({
+    var state = new fabric.Rect({
         left: 60,
         top: 60,
         width: 90,
         height: 90,
         type: 'state',
+        part: 'state',
         fill: '',
         stroke:'Black',
         originX: 'left',
@@ -100,7 +190,31 @@ function addState() {
         hasControls: true,
         hasRotatingPoint: false,
         lockRotation: true
-    }));
+    });
+
+    var text = new fabric.Text('function', {
+        left: 105,
+        top: 105,
+        type: 'state',
+        part: 'text',
+        fontSize: 15,
+        originX: 'center',
+        originY: 'center',
+        selectable: false,
+        hasBorders: false,
+        hasControls: false,
+        lockScalingX: true,
+        lockScalingY: true,
+        lockRotation: true,
+      });
+
+    var group = new fabric.Group([ state, text ], {
+        type: 'state'
+    });
+
+    group.text = text;
+
+    canvas.add(group);
 }
 
 // Adding a Transition to the GRID
@@ -132,9 +246,34 @@ function addTransition() {
         selectable: true,
         hasBorders: false,
         hasControls: false,
+        hasBorders: false,
+        hasControls: false,
+        lockScalingX: true,
+        lockScalingY: true,
+        lockRotation: true,
+        lockMovementX: true,
+        lockMovementY: true,
         type: 'transition',
         part: 'segment'
     });
+
+    var text = new fabric.Text('function', {
+        left: (line.get('x1') + line.get('x2')) / 2 + 30,
+        top: (line.get('y1') + line.get('y2')) / 2,
+        fontSize: 15,
+        originX: 'center',
+        originY: 'center',
+        selectable: true,
+        hasBorders: false,
+        hasControls: false,
+        lockScalingX: true,
+        lockScalingY: true,
+        lockRotation: true,
+        lockMovementX: true,
+        lockMovementY: true,
+        type: 'transition',
+        part: 'text',
+      });
 
     var centerX = (line.x1 + line.x2) / 2,
         centerY = (line.y1 + line.y2) / 2,
@@ -162,14 +301,19 @@ function addTransition() {
 
     line.circle = circle;
     line.arrow = arrow;
+    line.text = text;
 
     circle.line = line;
     circle.arrow= arrow;
+    circle.text = text;
 
     arrow.line = line;
     arrow.circle = circle;
+    arrow.text = text;
 
-    canvas.add(line, circle, arrow);
+    text.line = line;
+
+    canvas.add(line, circle, arrow, text);
 }
 
 function deleteList(listItems) {
@@ -216,18 +360,32 @@ document.addEventListener('keydown', function(event) {
             canvas.remove(activeObject);
         } else if(activeObject !== null && activeObject.type === "transition") {
             if(activeObject.part === "in") {
+                canvas.remove(activeObject.text);
                 canvas.remove(activeObject.line);
                 canvas.remove(activeObject.arrow);
                 canvas.remove(activeObject);
             } else if(activeObject.part === "out") {
+                canvas.remove(activeObject.text);
                 canvas.remove(activeObject.line);
                 canvas.remove(activeObject.circle);
                 canvas.remove(activeObject);
             } else {
+                canvas.remove(activeObject.text);
                 canvas.remove(activeObject.arrow);
                 canvas.remove(activeObject.circle);
                 canvas.remove(activeObject);
             }
+        }
+    } else if(keyPressed === 13) {
+        var activeObject = canvas.getActiveObject();
+        if(activeObject !== null && document.getElementById("rename").value !== "") {
+            if(activeObject.part === "text") {
+                activeObject.set('text', document.getElementById("rename").value);
+            } else {
+                activeObject.text.set('text', document.getElementById("rename").value);
+            }
+            document.getElementById("rename").value = "";
+            canvas.renderAll();
         }
     }
 });
